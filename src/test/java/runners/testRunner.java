3b0bc25f25a369e28fea27e85cec13f62c99cc4f
dheerajdevendrajain <dheerajdevendrajain@gmail.com>
2025-11-05 22:51:02 +0530

    Added LogUtilities to print logging in testcase execution

--- src/test/java/runners/testRunner.java
+++ src/test/java/runners/testRunner.java
@@ -2,7 +2,13 @@ package runners;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.apache.log4j.PropertyConfigurator;
 import org.junit.runner.RunWith;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
+
+import java.io.File;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -15,6 +21,24 @@ import org.junit.runner.RunWith;
         strict = true
 )
 
-public class testRunner {
+public class testRunner extends AbstractTestNGCucumberTests {
+
+    @BeforeSuite()
+    public void clearLogFile() {
+        String logFilePath = System.getProperty("user.dir") + File.separator + "test-output" + File.separator + "Logs" + File.separator + "test_automation.log";
+        File f = new File(logFilePath);
+        if (f.exists())
+            f.delete();
+
+        String log4jConfPath = System.getProperty("user.dir") + File.separator + "src" + File.separator + "test" + File.separator + "resources"
+                + File.separator + "config" + File.separator + "log4j.properties";
+        PropertyConfigurator.configure(log4jConfPath);
+    }
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 
 }
